065a357823fb7fc3e36ed03bfb2d7df60106efd4
Manny Garcia <mannysg88@gmail.com>
2022-02-25 12:54:40 -0600

    added variables for all available password characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment code here
+// Step 1. declare variables for characters that can be used in password
+//lowercase, uppercase, numbers, symbols
 
-
+//prettier-ignore
+var lowerCaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//prettier-ignore
+var upperCaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+//prettier-ignore
+var specialChar = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","["];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,7 +18,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
